72d9f3e454ccb9e0e1e8bbc3da3b28427fcc41ac
Wojciech Stróżyński <strozynski@procivis.ch>
2024-08-02 11:35:52 +0200

    ONE-2961: fixed JWT proof parsing

--- one-providers/src/exchange_protocol/openid4vc/mapper.rs
+++ one-providers/src/exchange_protocol/openid4vc/mapper.rs
@@ -189,13 +189,32 @@ fn value_to_model_claims(
     let mut model_claims = vec![];
 
     match json_value {
-        serde_json::Value::String(value) => {
+        serde_json::Value::String(_)
+        | serde_json::Value::Bool(_)
+        | serde_json::Value::Number(_) => {
+            let value = match json_value {
+                serde_json::Value::String(v) => v.to_owned(),
+                serde_json::Value::Bool(v) => {
+                    if *v {
+                        "true".to_string()
+                    } else {
+                        "false".to_string()
+                    }
+                }
+                serde_json::Value::Number(v) => v.to_string(),
+                _ => {
+                    return Err(OpenID4VCError::MappingError(
+                        "invalid value type".to_string(),
+                    ));
+                }
+            };
+
             model_claims.push(Claim {
                 id: Uuid::new_v4().into(),
                 credential_id,
                 created_date: now,
                 last_modified: now,
-                value: value.to_owned(),
+                value,
                 path: path.to_owned(),
                 schema: Some(claim_schema.to_owned()),
             });
